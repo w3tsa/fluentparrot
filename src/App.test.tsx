4f2689d405828249db27e08011896230975e7c35
Tanveer Sayem <tanveersayem@gmail.com>
2023-02-28 19:58:21 -0500

    Add ESLint, Prettier, and testing configuration files

---
+++ src/App.test.tsx
@@ -0,0 +1,30 @@
+import { describe, it, expect } from "vitest";
+import { render, screen } from "@testing-library/react";
+
+// components
+import App from "./App";
+
+describe("App", () => {
+  it("h1 is in the document", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    // Expect | Assert
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      }),
+    ).toBeInTheDocument();
+  });
+  it("renders h1 with text Fluent Parrot", () => {
+    // Arrange
+    render(<App />);
+    // Act
+    // Expect | Assert
+    expect(
+      screen.getByRole("heading", {
+        level: 1,
+      }),
+    ).toHaveTextContent("Fluent Parrot");
+  });
+});
